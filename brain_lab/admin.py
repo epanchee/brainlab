cedcc7d38e851a74872fd6a66cd6ec86ed2d37d4
Timofey <epanchee@gmail.com>
2016-01-30 18:05:28 +0500

    reformat code

--- brain_lab/admin.py
+++ brain_lab/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
+
 from brain_lab.models import Visitor, RegResult, SurveyList, PartInSurveys, Visit, Sibling
 
-for i in (Visitor, RegResult, SurveyList, PartInSurveys, Visit, Sibling):
-    admin.site.register(i)
+
+@admin.register(Visitor, RegResult, SurveyList, PartInSurveys, Visit, Sibling)
+class FooAdmin(admin.ModelAdmin):
+    pass
